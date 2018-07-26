62bf1791e7b2b543a725cb7a1c69cba63ba66453
Daren Lunsford <dlunsfo1@asu.edu>
2018-07-26 14:30:51 -0500

    Adds second api endpoint for website sections

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require('appoptics-apm');
 const serverless = require('serverless-http');
 const express = require('express');
 const axios = require('axios');
@@ -12,7 +11,19 @@ app.get('/', function(req, res) {
 
 //api/:id/:url
 
-app.get('/api/:id', (req, res) => {
+app.get('/api/:wss', (req, res) => {
+  const wssID = req.params.wss || 'Topics';
+  axios
+    .get(`http://blog.clickability.guru/templates/getWSS?wssID=${wssID}`)
+    .then(response => {
+      res.status(200).send(response.data);
+    })
+    .catch(err => {
+      console.log('err ', err);
+    });
+});
+
+app.get('/api/:id/:url', (req, res) => {
   const contentID = req.params.id || 487026341;
   axios
     .get(`http://www.clickability.guru/templates/getContent2?cID=${contentID}`)
